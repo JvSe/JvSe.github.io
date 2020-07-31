bc3ab02bc768eb9c0b47e6fd9d7f2ce4e09b7b3f
JvSe <joaovitorsoares12@gmail.com>
2020-07-31 17:22:40 -0300

    Updates

---
+++ precache-manifest.8c443f8ffb175e0c1e50358d2d0a2e6e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5d0b824382089d8e15750ad25e6c72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95d15299e199c3db3c44",
+    "url": "/static/css/main.038cb6ed.chunk.css"
+  },
+  {
+    "revision": "835f13403bc6bd275afc",
+    "url": "/static/js/2.fdc68540.chunk.js"
+  },
+  {
+    "revision": "95d15299e199c3db3c44",
+    "url": "/static/js/main.199ac868.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  },
+  {
+    "revision": "e4960c1d084132279f17ca869846c00c",
+    "url": "/static/media/estrelamario.e4960c1d.gif"
+  },
+  {
+    "revision": "0db44a8056b59a2eac16c12ff4f32df2",
+    "url": "/static/media/eu.0db44a80.png"
+  },
+  {
+    "revision": "30f25dae5e1be060e0fa725630113ba4",
+    "url": "/static/media/kakashi.30f25dae.gif"
+  },
+  {
+    "revision": "f835ca303f25b9ba450dec7bdfdcf3f6",
+    "url": "/static/media/kirito.f835ca30.gif"
+  },
+  {
+    "revision": "b4701a17a79b79cde4b0bbf133a2a9d2",
+    "url": "/static/media/minato.b4701a17.gif"
+  },
+  {
+    "revision": "2148c06016e17c618b074bdba52d5532",
+    "url": "/static/media/sonic.2148c060.gif"
+  },
+  {
+    "revision": "ef03498a3d6613e106ffb10e052e990a",
+    "url": "/static/media/supermario.ef03498a.gif"
+  },
+  {
+    "revision": "faf226d971910aac6d2068b39b1056e0",
+    "url": "/static/media/yang.faf226d9.gif"
+  },
+  {
+    "revision": "ac0fcf64393cd4b76061bab2bb9f75d3",
+    "url": "/static/media/zelda-link.ac0fcf64.gif"
+  }
+]);
